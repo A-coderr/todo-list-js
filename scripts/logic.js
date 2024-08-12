6d8fa9736a9df761876166be77220c8fb78bb2e7
Anzhelika Kostyuk <akostyuk@conestogac.on.ca>
2024-08-12 15:59:51 -0400

    Array logic
    Added an input field and a button.
    Store the user's inputs in an array.

---
+++ scripts/logic.js
@@ -0,0 +1,10 @@
+const todoList = [];
+
+function add(){
+    const inputName = document.querySelector('.todo-input');
+    const name = inputName.value;
+    
+
+    todoList.push(name);
+    console.log(todoList);
+}
