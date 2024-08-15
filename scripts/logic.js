de00510e3a4baeb4b7844a73825e92ecd932d6cc
Anzhelika Kostyuk <akostyuk@conestogac.on.ca>
2024-08-15 14:53:53 -0400

    Displays the todo list on the page

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,18 +1,25 @@
 const todoList = [];
-let todoListHTML = '';
+
+function displayTodoList(){
+
+    let todoListHTML = '';
+
+    for(let i = 0; i < todoList.length; i++){
+        const output = `<p>${todoList[i]}</p>`;
+        todoListHTML += output;
+    }
+    document.querySelector('.todo-list').innerHTML = todoListHTML;
+}
+
 
 function add(){
     const inputName = document.querySelector('.todo-input');
     const name = inputName.value;
     
-
     todoList.push(name);
     console.log(todoList);
 
-    inputName.value = '';
+    displayTodoList();
 
-    for(let i = 0; i < todoList.length; i++){
-        const html = `<p>${todoList[i]}</p>`;
-        todoListHTML += html;
-    }
+    inputName.value = '';
 }
