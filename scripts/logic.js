4b6f0fca7ded01807ff14bfea395d99a0b06a3c9
Anzhelika Kostyuk <akostyuk@conestogac.on.ca>
2024-08-15 16:56:39 -0400

    New css

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,11 +1,27 @@
-const todoList = [];
+const todoList = [{
+    name: '',
+    date: ''
+}];
 
 function displayTodoList(){
 
     let todoListHTML = '';
 
     for(let i = 0; i < todoList.length; i++){
-        const output = `<p>${todoList[i]}</p>`;
+        const value = todoList[i];
+
+        //Shortcut for this line
+        //const name = todoList.name;
+        const {name} = todoList;
+        const {date} = todoList;
+
+        const output = `
+            <
+            <p>
+            ${name} 
+            ${date}
+            <button onclick="delete(value)">Delete</button>
+            </p>`;
         todoListHTML += output;
     }
     document.querySelector('.todo-list').innerHTML = todoListHTML;
@@ -14,12 +30,21 @@ function displayTodoList(){
 
 function add(){
     const inputName = document.querySelector('.todo-input');
+    const inputDate = document.querySelector('.todo-date');
     const name = inputName.value;
+    const date = inputDate.value;
     
-    todoList.push(name);
-    console.log(todoList);
+    todoList.push({
+        name: name,
+        date: date,
+    });
 
     displayTodoList();
 
     inputName.value = '';
+}
+
+function delete(index){
+    todoList.splice(index, 1);
+    displayTodoList();
 }
