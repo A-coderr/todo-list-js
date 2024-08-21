bc61ec0d224e5bab39eeff828052800f48d82ef7
Anzhelika Kostyuk <akostyuk@conestogac.on.ca>
2024-08-21 16:16:53 -0400

    Finalized the functionality for the todo list

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,34 +1,40 @@
 const todoList = [{
-    name: '',
-    date: ''
-}];
+        name: 'make dinner',
+        date: '2022-12-22'
+    },
+    {
+        name: 'wash car',
+        date: '2024-05-12'
+    }];
+
+    
 
 function displayTodoList(){
 
     let todoListHTML = '';
 
-    for(let i = 0; i < todoList.length; i++){
-        const value = todoList[i];
+    /* todoList.forEach((todoObject, index) => {
 
-        //Shortcut for this line
-        //const name = todoList.name;
-        const {name} = todoList;
-        const {date} = todoList;
+    }) */
+
+    for(let i = 0; i < todoList.length; i++){
+
+        const value = todoList[i];
+        const {name, date} = value;
 
         const output = `
-            <
-            <p>
-            ${name} 
-            ${date}
-            <button onclick="delete(value)">Delete</button>
-            </p>`;
+            <div>${name}</div>
+            <div>${date}</div>                      
+            <button onclick="todo_delete(value)" class="delete-button">Delete</button>
+            `;
+
         todoListHTML += output;
     }
     document.querySelector('.todo-list').innerHTML = todoListHTML;
 }
 
 
-function add(){
+function todo_add(){
     const inputName = document.querySelector('.todo-input');
     const inputDate = document.querySelector('.todo-date');
     const name = inputName.value;
@@ -39,12 +45,14 @@ function add(){
         date: date,
     });
 
+    console.log(todoList)
+
     displayTodoList();
 
     inputName.value = '';
 }
 
-function delete(index){
+function todo_delete(index){
     todoList.splice(index, 1);
     displayTodoList();
 }
